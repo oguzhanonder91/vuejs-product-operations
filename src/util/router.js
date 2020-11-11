60f10445cb4e81d34a871c5b6a2bd2aac41e3621
oguzhanonder91 <oguzhanonder75@hotmail.com>
2020-11-11 14:09:49 +0300

    versions updated

--- src/util/router.js
+++ src/util/router.js
@@ -3,39 +3,63 @@ import VueRouter from 'vue-router';
 import store from '../store/store';
 import * as util from '../util/util';
 
-/*const ProductList = resolve  =>{
-  require.ensure(['../components/ProductList'] , () =>{
-    resolve (require("../components/ProductList"))
+
+const TheContainer = resolve => {
+  require.ensure(['../containers/TheContainer'], () => {
+    resolve(require("../containers/TheContainer"))
   })
-}
+};
 
-const ProductPurchase = resolve  =>{
-  require.ensure(['../components/ProductPurchase'] , () =>{
-    resolve (require("../components/ProductPurchase"))
+const DashBoard = resolve => {
+  require.ensure(['../components/Dashboard'], () => {
+    resolve(require("../components/Dashboard"))
   })
-}
+};
 
-
-const ProductSell = resolve  =>{
-  require.ensure(['../components/ProductSell'] , () =>{
-    resolve (require("../components/ProductSell"))
+const UserConfirmation = resolve => {
+  require.ensure(['../components/UserConfirmation'], () => {
+    resolve(require("../components/UserConfirmation"))
   })
-}
+};
 
-const Login = resolve  =>{
-  require.ensure(['../components/Login'] , () =>{
-    resolve (require("../components/Login"))
+const ProductPurchase = resolve => {
+  require.ensure(['../components/ProductPurchase'], () => {
+    resolve(require("../components/ProductPurchase"))
   })
-}
+};
 
-
-const Register = resolve  =>{
-  require.ensure(['../components/Register'] , () =>{
-    resolve (require("../components/Register"))
+const ProductSell = resolve => {
+  require.ensure(['../components/ProductSell'], () => {
+    resolve(require("../components/ProductSell"))
   })
-}
-*/
-const DashBoard = () => System.import('../components/Dashboard');
+};
+
+const Login = resolve => {
+  require.ensure(['../components/Login'], () => {
+    resolve(require("../components/Login"))
+  })
+};
+
+
+const Register = resolve => {
+  require.ensure(['../components/Register'], () => {
+    resolve(require("../components/Register"))
+  })
+};
+
+const ForgotPassword = resolve => {
+  require.ensure(['../components/ForgotPassword'], () => {
+    resolve(require("../components/ForgotPassword"))
+  })
+};
+
+const ChangePassword = resolve => {
+  require.ensure(['../components/ChangePassword'], () => {
+    resolve(require("../components/ChangePassword"))
+  })
+};
+
+/*const DashBoard = () => System.import('../components/Dashboard');
 const TheContainer = () => System.import('../containers/TheContainer');
 const ProductPurchase = () => System.import('../components/ProductPurchase');
 const ProductSell = () => System.import('../components/ProductSell');
@@ -43,7 +67,7 @@ const UserConfirmation = () => System.import('../components/UserConfirmation');
 const Login = () => System.import('../components/Login');
 const Register = () => System.import('../components/Register');
 const ForgotPassword = () => System.import('../components/ForgotPassword');
-const ChangePassword = () => System.import('../components/ChangePassword');
+const ChangePassword = () => System.import('../components/ChangePassword');*/
 
 Vue.use(VueRouter);
 
